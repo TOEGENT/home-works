c1c3ad6bfda2c7ea79a523fa4bc71d9a4c770064
TOEGENT <119882568+TOEGENT@users.noreply.github.com>
2025-09-24 19:10:16 +0500

    Update home_work1.py

--- home_work1.py
+++ home_work1.py
@@ -41,3 +41,21 @@ for i,key in enumerate(counts,1):
     if i==3:
         break
 """
+
+#4
+"""
+N = int(input("Введите супремум"))
+l_raw = [i for i in range(2, N)]
+l_ready=[]
+def simp(l_raw,l_ready):
+    if len(l_raw)==0:
+        return l_ready
+    l_ready.append(l_raw[0])
+    temp = set([j for j in range(2,N) if j%l_raw[0]==0])
+
+    l_raw=set(l_raw)
+    l_raw-=temp
+    simp(list(l_raw),l_ready)
+simp(l_raw,l_ready)
+print(l_ready)
+"""
