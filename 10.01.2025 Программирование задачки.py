4e3faa4ddbd6e689f664ff9a03f4ccd27883c40c
TOEGENT <119882568+TOEGENT@users.noreply.github.com>
2025-10-05 22:46:23 +0500

    Add files via upload

--- 10.01.2025 Программирование задачки.py
+++ 10.01.2025 Программирование задачки.py
@@ -135,7 +135,29 @@ if spec_allow=="Y":
 shuffle(result)
 print("".join(result))
 """
+"""
+MCMLXXXIV
+1) 5 (V)
+2) 1 - I<V (V) -> 5-1=4
+3) X - X>V (X) -> 4+10 = 14
+4) X - X=X (X) -> 14+10 = 24
+5) X - X=X (X) -> 24+10 = 34
+6) L - L>X (L) -> 34+50 = 84
+7) M -> M>L (M) -> 84+1000 = 1084
+8) C -> C<M (M) -> 1084-100 = 984
+9) M -> M=M (M) -> 984+1000 = 1984
+
+1984
+
+1) 4 -> V -> 5-4=1 -> I -> IV
+2) 80 -> C -> 100-80=20 -> 20 not in roman.values -> L -> 80-50 = 30 not in roman.values -> closest div = 10 -> 30/10 = 3 -> XXX -> LXXX
+3) 900 -> M -> 1000-900 -> 100 -> C -> CM
+4) 1000 - M
+result = MCMLXXXIV
+"""
+
 #5 rimsky cool
+"""
 def converter(roman_tests):
     roman_to_int = {
         'I': 1,
@@ -144,12 +166,98 @@ def converter(roman_tests):
         'L': 50,
         'C': 100,
         'D': 500,
-        'M': 1000
+        'M': 1000,
+    }
+    int_to_roman = {
+        1: 'I',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+        500: 'D',
+        1000: 'M'
     }
     result = []
     for num in roman_tests:
         if isinstance(num,str):
-            most = "I"
-            for i in num:
-                if roman_to_int[i]>=most:
-                    result
+            num=num[::-1]
+            most = num[0]
+            total = roman_to_int[num[0]]
+            for i in num[1:]:
+                if roman_to_int[i]>=roman_to_int[most]:
+                    total+=roman_to_int[i]
+                    most = i
+                else:
+                    total-=roman_to_int[i]
+            result.append(total)
+        else:
+            total=""
+            snum = str(num)[::-1]
+            c=0
+            for i in snum:
+                if i=="0":
+                    c+=1
+                    continue
+                i+=c*"0"
+                l = sorted([(abs(int(i)-j),j) for j in roman_to_int.values()])
+                closest_plus = l[0]
+                remain = closest_plus[0]
+                closest = closest_plus[1]
+                if int(i) in int_to_roman.keys():
+                    total=int_to_roman[int(i)]+total
+                elif remain in int_to_roman.keys():
+                    if closest<int(i):
+                        total = int_to_roman[closest] + int_to_roman[remain] +  total
+                    else:
+                        total=int_to_roman[remain]+int_to_roman[closest]+total
+                else:
+                    if closest>int(i):
+                        second_closest_plus = l[1]
+                        remain = second_closest_plus[0]
+                        closest = second_closest_plus[1]
+                        if remain in int_to_roman.keys():
+                            total +=int_to_roman[closest]+int_to_roman[remain]
+
+                    closest_div = 0
+                    mod=float("inf")
+                    for i in sorted(list(int_to_roman.keys()),reverse=True):
+                        if remain%i==0:
+                            closest_div=i
+                            break
+                    total =int_to_roman[closest] + remain//closest_div*int_to_roman[closest_div] + total # остаток допустим 30 (80-50) разбиваем на 3*10 = XXX
+                c+=1
+            result.append(total)
+    return result
+print(converter([i for i in range(1,1000)]))
+print(converter(converter([i for i in range(1,1000)])))
+"""
+words = ["лололошка", "бананы"]
+#deadman
+from random import choice
+def play_deadman(tryes=8):
+    word = words[choice(range(len(words)))]
+    guess_total = ["_" for i in range(len(word))]
+    print("Добро пожаловать в висельницу! Ваша задача - угадать слово быстрее, чем человек успеет повесится. Удачи!")
+
+    for i in range(tryes*len(word)):
+        print("".join(guess_total))
+
+        inp1 = input("Введите номер и предполагаемую букву (по типу: 1-а) ").split("-")
+        guess_index = int(inp1[0])-1
+        guess_symbol = inp1[1]
+        if guess_index>=len(word):
+            print("Нет такого номера!")
+            continue
+        if word[guess_index]==guess_symbol:
+            guess_total[guess_index]=guess_symbol
+            print("Угадали!")
+        else:
+            print("Неугадали!")
+        if "".join(guess_total)==word:
+            print("Вы угадали слово! Поздравляю!")
+            return 1
+    else:
+        print(f"Вы так долго угадывали слово, что человек уже успел повесится! Заданное слово было {word}")
+        return 0
+play_deadman()
+
